fb16ddd6125a0ec7ad73dba76a698951eb8adbb5
Arthur Bauer <arthur.bauer@skf.com>
2015-12-16 09:31:04 +0100

    First running version of the script

---
+++ ReleaseNoteMaker.php
@@ -0,0 +1,29 @@
+<?php
+
+$tags=`git tag | grep ^v | sort `; // get all the tags that start with a "v"
+$tags=explode("\n",$tags);
+
+$counter=count($tags)-2;
+
+$tags[$counter+1]="HEAD"; // and the current HEAD as last "version"
+
+//start output
+echo "# Release notes for ";
+echo `pwd`;
+
+// loop through all the tags and create an output for that version
+for ($i=0;$i<=$counter;$i++)
+{
+$j=$i+1;
+echo "\n## $tags[$j]\n";
+echo "Changes from $tags[$i] - $tags[$j]\n\n";
+
+// This command creates the actual release note, greps out commits that contain "(minor)" and "Todo"
+// Feel free to add more greps if needed (e.g. for "version number update" etc)
+$com="git log --no-merges --pretty=format:\"* %s\" ".$tags[$i]."..".$tags[$j]." | grep -v \(minor\) | grep -vi \"Todo\"";
+echo `$com`;
+}
+
+
+
+?>
